84979c7049d4b16ab4a00ed573b7e14e37103cec
THATONEKO <thatoneko03@gmail.com>
2025-06-19 18:46:29 +0200

    connect the recording tables

--- src/app/dashboard/secondary/page.tsx
+++ src/app/dashboard/secondary/page.tsx
@@ -1,8 +1,58 @@
-export default function SecondaryUserDashboard(){
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-gray-600 text-2xl font-bold">Secondary User Dashboard</h1>
-            <p className="text-gray-600">welcome, User! You have limited access.</p>
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function SecondaryDashboard() {
+    const router = useRouter();
+
+    const handleNavigate = (path: string) => {
+        router.push('/dashboard/secondary/sports/${path}');
+    };
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-6">Secodary User Dashboard</h1>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <button
+                    onClick={() => handleNavigate("basketball")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-blue-100"              
+                >
+                    Record Basketball Scores
+                </button>
+
+                <button
+                    onClick={() => handleNavigate("soccer")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-purple-100"
+                >
+                    Record Soccer Scores
+                </button>
+                <button
+                    onClick={() => handleNavigate("racing")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-purple-100"
+                >
+                    Record Springting Scores
+                </button>
+                <button
+                    onClick={() => handleNavigate("speedwalk")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-purple-100"
+                >
+                    Record Speed Walk
+                </button>
+                <button 
+                    onClick={() => handleNavigate("shortput")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-purple-100"
+                >
+                    Record Short Put
+                </button>
+                <button
+                    onClick={() => handleNavigate("longjump")}
+                    className="text-black p-4 bg-white rounded shadow hover:bg-red-100"
+                >
+                    Record Long jump
+                </button>
+            </div>
+
         </div>
     )
 }
